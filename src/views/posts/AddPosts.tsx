6edfe9c23a8cd9018229e822d89c060d4ed65842
Chinthana Weligamage <chinthanaweligamage1@gmail.com>
2025-05-06 13:29:50 +0530

    feat: add AddPostModal component and integrate it into ViewPosts; update dialog dependency version

---
+++ src/views/posts/AddPosts.tsx
@@ -0,0 +1,44 @@
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+
+const AddPostModal = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button
+          size={"lg"}
+          className="flex flex-row items-center justify-center gap-3"
+        >
+          <Plus />
+          Add Post
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-md">
+        <DialogHeader>
+          <DialogTitle>Add Post</DialogTitle>
+          <DialogDescription>Add Post form will be here</DialogDescription>
+        </DialogHeader>
+        <div className="flex items-center space-x-2"></div>
+        <DialogFooter className="sm:justify-start">
+          <DialogClose asChild>
+            <Button type="button" variant="secondary">
+              Close
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AddPostModal;
--- src/views/posts/ViewPosts.tsx
+++ src/views/posts/ViewPosts.tsx
@@ -1,11 +1,12 @@
 import PostCard from "@/components/cards/PostCard";
+import AddPostModal from "./AddPosts";
 
 const samplePosts = [
   {
     title: "Beautiful sunset at the beach",
     description: "Chasing the sun and making memories with my loved ones",
     imageUrls: [
-      "https://images.unsplash.com/photo-1514963019544-64b2f377c1a4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80",
+      "https://plus.unsplash.com/premium_photo-1661957387235-3bc814072fb3?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     ],
   },
   {
@@ -20,7 +21,7 @@ const samplePosts = [
     title: "A peaceful walk in the forest",
     description: "Being surrounded by nature and the sound of birds chirping",
     imageUrls: [
-      "https://images.unsplash.com/photo-1514963019544-64b2f377c1a4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80",
+      "https://images.unsplash.com/photo-1425913397330-cf8af2ff40a1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     ],
   },
 ];
@@ -28,6 +29,9 @@ const samplePosts = [
 const ViewPosts = () => {
   return (
     <div className="flex flex-col items-center w-full max-w-2xl mx-auto my-10 gap-6 px-4 overflow-y-auto">
+      <div className="fixed top-10 right-10 z-50">
+        <AddPostModal />
+      </div>
       {samplePosts.map((post, index) => (
         <PostCard key={index} data={post} />
       ))}
